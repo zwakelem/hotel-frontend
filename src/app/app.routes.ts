dd1c0595c82e6ed029e3fac05070030c9365d503
Zwaks <zwakelemgabhi@gmail.com>
2025-08-30 06:20:31 +0200

    add profile and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Register } from './register/register';
+import { Profile } from './profile/profile';
+import { Guard } from './service/guard';
+import { EditProfile } from './edit-profile/edit-profile';
 
 export const routes: Routes = [
   {
@@ -10,5 +13,19 @@ export const routes: Routes = [
   {
     path: 'register',
     component: Register,
+  },
+  {
+    path: 'profile',
+    component: Profile,
+    canActivate: [Guard],
+  },
+  {
+    path: 'edit-profile',
+    component: EditProfile,
+    canActivate: [Guard],
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
   },
 ];
