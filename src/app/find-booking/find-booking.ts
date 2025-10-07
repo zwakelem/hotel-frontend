f9d3fa6fda2e4e9b9a0f6bf5f3f5d5f5a44d7649
Zwaks <zwakelemgabhi@gmail.com>
2025-10-07 11:27:35 +0200

    update to profile view

--- src/app/find-booking/find-booking.ts
+++ src/app/find-booking/find-booking.ts
@@ -1,11 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Api } from '../service/api';
 
 @Component({
   selector: 'app-find-booking',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './find-booking.html',
-  styleUrl: './find-booking.css'
+  styleUrl: './find-booking.css',
 })
 export class FindBooking {
+  constructor(private apiService: Api) {}
 
+  confirmationCode: string = '';
+  bookingDetails: any = null;
+  error: any = null;
+
+  handleSearch() {
+    if (!this.confirmationCode.trim()) {
+      this.showError('Please enter the booking confirmation code');
+      return;
+    }
+
+    this.apiService.getBookingByReference(this.confirmationCode).subscribe({
+      next: (res) => {
+        console.log(res.booking);
+        this.bookingDetails = res.booking;
+      },
+      error: (err) => {
+        this.showError(err?.error.message || 'Error fetching booking details');
+      },
+    });
+  }
+
+  showError(err: any): void {
+    console.log(err);
+    this.error = err;
+    setTimeout(() => {
+      this.error = '';
+    }, 4000);
+  }
 }
