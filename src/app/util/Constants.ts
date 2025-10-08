14707d06d2ca1a5865ec31ba02cd42bd4c8de648
Zwaks <zwakelemgabhi@gmail.com>
2025-10-08 21:31:27 +0200

    small changes

--- src/app/util/Constants.ts
+++ src/app/util/Constants.ts
@@ -1,4 +1,5 @@
 export class Constants {
+  // TODO: add this to local storage
   static readonly roomTypes: string[] = ['SINGLE', 'DOUBLE', 'SUITE', 'TRIPLE'];
   static readonly BASE_URL = 'http://localhost:8080/api';
   static readonly ENCRYPTION_KEY = 'dennis-encrypt-key';
